8f30d1f99c4858fe9e3db1db4c7e03a824ae907f
olgunbey <olgunsahin0161@hotmail.com>
2022-10-21 01:56:34 +0300

    Table Per Hiearachy

---
+++ Table Per Hieararchy/Migrations/20221018180635_mig.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Table_Per_Hieararchy.Migrations
+{
+    public partial class mig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BaskanEntities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BaskanOz = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UyeOz = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    YardımciOz = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BaskanEntities", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BaskanEntities");
+        }
+    }
+}
---
+++ Table Per Hieararchy/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+{
+    DbXContext dbXContext = new();
+    BaskanEntity baskan = new() { BaskanOz = "baskan", Name = "eren",CreateDate=DateTime.Now };
+    dbXContext.BaskanEntities.Add(baskan);
+    dbXContext.SaveChanges();
+    //BaseEntity entity = new()
+    //{
+    //    CreateDate = DateTime.Now
+    //};
+}
+public  class BaseEntity
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public virtual DateTime CreateDate { get; set; }
+
+}
+public class BaskanEntity:BaseEntity
+{
+    public string BaskanOz { get; set; }
+   
+    public override DateTime CreateDate { get => base.CreateDate; set => base.CreateDate = value; }
+}
+public class UyeEntity:BaskanEntity
+{
+    public string UyeOz { get; set; }
+    //public override DateTime CreateDate { get => base.CreateDate; set => base.CreateDate = value; }
+
+}
+public class YardımcıOz:BaskanEntity
+{
+    public string YardımciOz { get; set; }
+    //public override DateTime CreateDate { get => base.CreateDate; set => base.CreateDate = value; }
+
+}
+public class DbXContext:DbContext
+{
+    public DbSet<BaskanEntity> BaskanEntities { get; set; }
+    public DbSet<UyeEntity> UyeEntities { get; set; }
+    public DbSet<YardımcıOz> YardımcıOz { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDb; Initial Catalog=TphKlupMantigi");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BaskanEntity>().Ignore(p => p.CreateDate);
+    }
+}
+
